71bc9bcbb00aab8533da71b1e1dd51e7335daeda
Dang Mai <contact@dangmai.net>
2018-05-16 18:19:09 -0400

    Rename dev server file

--- src/commands/dev.ts
+++ src/commands/dev.ts
@@ -1,5 +1,5 @@
 import {Command, flags} from '@oclif/command'
-import runDevServer from '../libs/server'
+import runDevServer from '../libs/dev-server'
 
 export default class Dev extends Command {
   static description = 'Running Moonglow Dev Server'
