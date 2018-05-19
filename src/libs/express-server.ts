a1b621eed03e84ba02be264379c670aba1dbdfa2
Dang Mai <contact@dangmai.net>
2018-05-19 16:44:04 -0400

    Refactor code to create router

--- src/libs/express-server.ts
+++ src/libs/express-server.ts
@@ -1,15 +1,13 @@
 import * as express from 'express'
 import * as React from 'react'
-import createRouter, {Route} from 'router5/create-router'
+import {Route} from 'router5/create-router'
 import runReactServer from './react-server'
-import {browserPlugin, listenersPlugin} from './router'
+import {getRouter} from './router'
 
 export default (routes: Route[], appComponent: React.ComponentClass): express.Express => {
   const app = express()
   app.get('*', (req, res, next) => {
-    const router = createRouter(routes)
-      .usePlugin(browserPlugin({useHash: false}))
-      .usePlugin(listenersPlugin())
+    const router = getRouter(routes)
     router.start(req.originalUrl, (error, _state) => {
       if (error) {
         next()
--- src/libs/router.ts
+++ src/libs/router.ts
@@ -1,6 +1,12 @@
-import createRouter from 'router5/create-router'
+import createRouter, {Route, Router} from 'router5/create-router'
 import browserPlugin from 'router5/plugins/browser'
 import listenersPlugin from 'router5/plugins/listeners'
 
 export {Link, routeNode, RouterProvider, withRoute} from 'react-router5'
 export {browserPlugin, createRouter, listenersPlugin}
+
+export function getRouter(routes: Route[]): Router {
+  return createRouter(routes)
+    .usePlugin(browserPlugin({useHash: false}))
+    .usePlugin(listenersPlugin())
+}
