c17ea9a3e4ce43c900c8f3af27d8f9371e010e43
Dang Mai <contact@dangmai.net>
2018-05-21 18:26:24 -0400

    Fix routes template

--- templates/routes.js
+++ templates/routes.js
@@ -1,4 +1,4 @@
-import {MoonglowRouter, ReactRouterProvider} from 'moonglow/lib/src/libs/router';
+import {ExpressRouterProvider, HttpMethod, MoonglowRouter, ReactRouterProvider} from 'moonglow/lib/src/libs/router';
 import App from './pages/app';
 
 const router = new MoonglowRouter();
@@ -8,9 +8,9 @@ router.use(reactProvider);
 router.use(expressProvider);
 
 class Routes {
-  static home = reactProvider.route('home', '/')
-  static about = reactProvider.route('about', '/about')
-  static server = expressProvider.route(HttpMethod.ALL, '/server', (req, res, next) => { res.send('Hi') })
+  static home = reactProvider.route('home', '/');
+  static about = reactProvider.route('about', '/about');
+  static server = expressProvider.route(HttpMethod.ALL, '/server', (req, res, next) => { res.send('Hi') });
 }
 
 export default Routes;
